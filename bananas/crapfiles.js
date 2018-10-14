f1b59939171eb020efbc08f7001fc2f490a2bcdb
irmarivas <irmarivas79@gmail.com>
2018-10-13 18:59:31 -0700

    I created my morning routine

--- bananas/crapfiles.js
+++ bananas/crapfiles.js
@@ -1,1 +1,46 @@
-blah blah blah
+"use strict";
+
+//morning routine
+
+function morningRoutine()
+{
+   console.log(`my morning routine`);
+    gettingOutofBed();
+    goingToshower();
+    gettingDressed();
+    drinkingCoffee();
+    brushTeeth();
+
+}
+
+//get out of bed
+function gettingOutofBed()
+{
+    console.log(`getting out of bed`);
+}
+
+//shower
+function goingToshower()
+{
+    console.log(`going to shower`); 
+}
+
+//get dressed
+function gettingDressed()
+{
+    console.log(`getting dressed`);
+}
+
+//drink coffee
+function drinkingCoffee()
+{
+    console.log(`drinking coffee`);
+}
+
+//brush teeth
+function brushTeeth()
+{
+    console.log(`brushteeth`);
+}
+
+morningRoutine();

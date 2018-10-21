a7d96bbe38050e0c7965662679decf05b33e5924
irmarivas <irmarivas79@gmail.com>
2018-10-21 15:40:11 -0700

    I created smart phone method and made an instance called iphone

--- objects/smartPhone.js
+++ objects/smartPhone.js
@@ -4,4 +4,11 @@ function Smartphone(texts, apps, phoneCalls, photos)
     this.apps = apps;
     this.phoneCalls = phoneCalls;
     this.photos = photos;
+    this.text = function()
+    {
+        console.log(`hello`);
+    };
 }
+
+const iphone = new Smartphone([`hello`, `what up`, `sup`], [`siri`, `google`,`twitter`], 3, ["pepper.jpg","mom.png","birthday.jpg"]);
+console.log( iphone.text() );
